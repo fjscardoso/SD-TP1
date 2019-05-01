9fe33517c6783953be71dc0d0c88d81da6651341
fjscardoso <fj.cardoso@campus.fct.unl.pt>
2019-05-01 23:52:12 +0100

    Revert "commit"
    This reverts commit 325c1767

--- SD2019-Microgram-Code/src/microgram/impl/srv/soap/_TODO_PostsWebService.java
+++ SD2019-Microgram-Code/src/microgram/impl/srv/soap/_TODO_PostsWebService.java
@@ -1,15 +1,13 @@
 package microgram.impl.srv.soap;
 
 import microgram.api.Post;
-import microgram.impl.srv.java.Posts;
+import microgram.api.java.Posts;
 import microgram.api.soap.MicrogramException;
 import microgram.api.soap.SoapPosts;
 import microgram.impl.srv.java.JavaPosts;
 
-import java.util.List;
-
 //Make this class concrete.
-public class _TODO_PostsWebService extends SoapService implements SoapPosts {
+public abstract class _TODO_PostsWebService extends SoapService implements SoapPosts {
 
 	final Posts impl;
 	
@@ -21,35 +19,5 @@ public class _TODO_PostsWebService extends SoapService implements SoapPosts {
 	public Post getPost( String postId ) throws MicrogramException {
 		return super.resultOrThrow( impl.getPost(postId));
 	}
-
-	@Override
-	public void deletePost(String postId) throws MicrogramException {
-		super.resultOrThrow( impl.deletePost(postId));
-	}
-
-	@Override
-	public String createPost(Post post) throws MicrogramException {
-		return null;
-	}
-
-	@Override
-	public boolean isLiked(String s, String s1) throws MicrogramException {
-		return false;
-	}
-
-	@Override
-	public void like(String s, String s1, boolean b) throws MicrogramException {
-
-	}
-
-	@Override
-	public List<String> getPosts(String s) throws MicrogramException {
-		return null;
-	}
-
-	@Override
-	public List<String> getFeed(String s) throws MicrogramException {
-		return null;
-	}
-
+	
 }
